eb025169d20fa963d36932754d30f556373daefa
ZmahHata <36806397+ZmahHata@users.noreply.github.com>
2018-03-04 12:35:59 +0800

    Add files via upload

--- MyThread.java
+++ MyThread.java
@@ -3,31 +3,24 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mythread;
+package mysleep;
+
+import static java.lang.Thread.sleep;
 
 /**
  *
  * @author master lab
  */
-
-class MyThread extends Thread {
-
+public class MyThread extends Thread{
     public void run() {
-        System.out.println("Hye Zmah Hata");
-        System.out.println("Welcome to STIW3054 subject");
-    }
-
-    public static void main(String args[]) {
-        MyThread t1 = new MyThread();
-        MyThread t2 = new MyThread();
-        System.out.println("Name of t1:" + t1.getName());
-        System.out.println("Name of t2:" + t2.getName());
-        System.out.println("id of t1:" + t1.getId());
-
-        t1.start();
-        t2.start();
-
-        t1.setName("STIW3054");
-        System.out.println("After changing name of t1:" + t1.getName());
+        System.out.println(Thread.currentThread().getName());
+        try {
+            for (int x = 0; x <= 10; x++) {
+                System.out.println(x);
+                sleep(1000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
